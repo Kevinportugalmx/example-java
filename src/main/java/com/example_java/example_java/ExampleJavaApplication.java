35601f744472fa7d1969f3c55f7954e6a12ae346
Kevin Portugal <kevinportugalmx@gmail.com>
2024-10-08 14:15:55 -0700

    baseapp

---
+++ src/main/java/com/example_java/example_java/ExampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example_java.example_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example_java/example_java/ExampleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_java.example_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
